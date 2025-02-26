8544be525a61d68835060427adad9845612f82e3
DJBigH <khanhbeotixiu9x@gmail.com>
2025-02-26 17:03:43 +0700

    50% Best Rating

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //Main Slider
     $(".main__slide").owlCarousel({
         loop:true,
         nav:false,
@@ -8,4 +9,24 @@ $(document).ready(function(){
         autoplayTimeout: 3000,
         autoplayHoverPause: true,
     });
+
+    // Products box Slider
+    $('.product-box-lists').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
   });
+
